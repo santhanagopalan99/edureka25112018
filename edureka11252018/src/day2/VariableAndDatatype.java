b7e32df146ef914bfc951227392f1690bbee4e02
santhanagopalan <santhanagopalan@santhandumsmbp2>
2018-11-25 10:10:45 -0600

    test

---
+++ edureka11252018/src/day2/VariableAndDatatype.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class VariableAndDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstnum;
+		firstnum=20;
+		String myname="Santy";
+System.out.println("I am printing string "+ firstnum++);
+System.out.println("I am printing string "+ ++firstnum);
+
+	}
+
+}
